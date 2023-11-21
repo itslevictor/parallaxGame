addd1eb9d890556efa8b3b394597f2231523aa4d
Victor <adm.victorfernando@gmail.com>
2023-11-21 17:57:54 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,66 @@
+var player = document.getElementById('player');
+var speed = 5;
+var backgrounds = document.getElementsByClassName('background');
+var keys = {};
+
+document.getElementById('play').addEventListener('click', function() {
+    document.getElementById('menu').style.display = 'none';
+    document.getElementById('game').style.display = 'block';
+    player.style.top = window.innerHeight / 2 - player.offsetHeight / 2 + 'px';
+    player.style.left = window.innerWidth / 2 - player.offsetWidth / 2 + 'px';
+    for (var i = 0; i < backgrounds.length; i++) {
+        backgrounds[i].classList.add('parallax');
+    }
+});
+
+document.getElementById('select-skin').addEventListener('click', function() {
+    document.getElementById('menu').style.display = 'none';
+    document.getElementById('skins').style.display = 'block';
+});
+
+document.getElementById('blue-skin').addEventListener('click', function() {
+    player.style.backgroundImage = 'url(https://i.pinimg.com/736x/dd/b0/a4/ddb0a4f27b9be7ffedf03a872545a3e2.jpg)';
+    document.getElementById('skins').style.display = 'none';
+    document.getElementById('menu').style.display = 'block';
+});
+
+document.getElementById('pink-skin').addEventListener('click', function() {
+    player.style.backgroundImage = 'url(https://i.pinimg.com/736x/dd/b0/a4/ddb0a4f27b9be7ffedf03a872545a3e2.jpg)';
+    document.getElementById('skins').style.display = 'none';
+    document.getElementById('menu').style.display = 'block';
+});
+
+document.getElementById('back-to-menu').addEventListener('click', function() {
+    document.getElementById('game').style.display = 'none';
+    document.getElementById('menu').style.display = 'block';
+    for (var i = 0; i < backgrounds.length; i++) {
+        backgrounds[i].classList.remove('parallax');
+    }
+});
+
+window.addEventListener('keydown', function(event) {
+    keys[event.key] = true;
+});
+
+window.addEventListener('keyup', function(event) {
+    keys[event.key] = false;
+});
+
+function gameLoop() {
+    if (keys['w'] || keys['ArrowUp']) {
+        player.style.top = (parseInt(player.style.top) - speed) + 'px';
+    }
+    if (keys['a'] || keys['ArrowLeft']) {
+        player.style.left = (parseInt(player.style.left) - speed) + 'px';
+    }
+    if (keys['s'] || keys['ArrowDown']) {
+        player.style.top = (parseInt(player.style.top) + speed) + 'px';
+    }
+    if (keys['d'] || keys['ArrowRight']) {
+        player.style.left = (parseInt(player.style.left) + speed) + 'px';
+    }
+
+    requestAnimationFrame(gameLoop);
+}
+
+gameLoop();
